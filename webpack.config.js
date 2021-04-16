451be5e2057a72d2601aa3309c322fb59d3994f1
Mikael P. Messias <mikaelpmessias@gmail.com>
2021-04-16 12:57:56 -0300

    refactor: update components alias and add root alias
    Signed-off-by: Mikael P. Messias <mikaelpmessias@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
     alias: {
+      '~': path.resolve(__dirname, 'src/'),
       '@Components': path.resolve(__dirname, 'src/components'),
       '@Assets': path.resolve(__dirname, 'src/assets'),
       '@Pages': path.resolve(__dirname, 'src/pages'),
