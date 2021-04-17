df8db02bee0c728b470e03b6974267beebfb399f
Mikael P. Messias <mikaelpmessias@gmail.com>
2021-04-17 16:57:33 -0300

    refactor(LoadingOverlay): update zindex and animation
    Signed-off-by: Mikael P. Messias <mikaelpmessias@gmail.com>

--- src/components/LoadingOverlay/LoadingOverlay.jsx
+++ src/components/LoadingOverlay/LoadingOverlay.jsx
@@ -1,13 +1,17 @@
 import './LoadingOverlay.scss';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 import Pokeball from '@Assets/images/pokeball.png';
 
 export const LoadingOverlay = (props) => {
   const { isLoading } = props;
 
-  return isLoading && (
-    <div className="loading-overlay">
+  return (
+    <div className={classnames(
+      "loading-overlay", {
+        'is--loading': isLoading,
+      })}>
       <img
         className="loading-overlay__spinner"
         src={Pokeball}
