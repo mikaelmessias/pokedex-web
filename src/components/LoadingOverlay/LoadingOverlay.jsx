19fa39ea57121616fb4ef5cd424c4caf7f5a4f27
Mikael P. Messias <mikaelpmessias@gmail.com>
2021-04-17 19:57:41 -0300

    refactor(LoadingOverlay): add scroll to top effect when loading
    Signed-off-by: Mikael P. Messias <mikaelpmessias@gmail.com>

--- src/components/LoadingOverlay/LoadingOverlay.jsx
+++ src/components/LoadingOverlay/LoadingOverlay.jsx
@@ -3,9 +3,19 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 import Pokeball from '@Assets/images/pokeball.png';
+import { useEffect } from 'react';
 
 export const LoadingOverlay = (props) => {
   const { isLoading } = props;
+
+  useEffect(() => {
+    if (isLoading) {
+      window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'initial';
+    }
+  }, [isLoading]);
 
   return (
     <div className={classnames(
