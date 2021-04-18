36b92be521358ae5893a3606a336204830f07a6e
Mikael P. Messias <mikaelpmessias@gmail.com>
2021-04-17 22:59:42 -0300

    refactor(home): add mobile styles to PokemonList and home page
    Signed-off-by: Mikael P. Messias <mikaelpmessias@gmail.com>

--- src/pages/Home/components/PokemonList/PokemonList.jsx
+++ src/pages/Home/components/PokemonList/PokemonList.jsx
@@ -1,21 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
-import { fetchOnePokemon } from "@Store/ducks/pokemon";
+import { useSelector } from "react-redux";
+import { PokemonCard } from '../PokemonCard/PokemonCard';
 import "./PokemonList.scss";
 
 export const PokemonList = () => {
-  const dispatch = useDispatch();
   const { pokemons } = useSelector((state) => state.pokemon);
 
   return (
     <ul className="pokemon-list">
       {pokemons.map((pokemon) => (
-        <li className="pokemon-list__item" key={pokemon.name}>
-          <button
-            onClick={() => dispatch(fetchOnePokemon(pokemon.url))}
-          >
-            {pokemon.name}
-          </button>
-        </li>
+        <PokemonCard
+          key={pokemon.name}
+          pokemon={pokemon}
+        />
       ))}
     </ul>
   );
