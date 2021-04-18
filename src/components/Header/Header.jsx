98b0036d658987e382d3e6b001fae632d8c27575
Mikael P. Messias <mikaelpmessias@gmail.com>
2021-04-18 14:01:42 -0300

    refactor(header): add children on render
    Signed-off-by: Mikael P. Messias <mikaelpmessias@gmail.com>

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,19 @@ import PropTypes from 'prop-types';
 import PokeDexLogo from '~/assets/images/logo.png';
 import './Header.scss';
 
-export const Header = ({ className }) => {
+export const Header = ({ className, children }) => {
   return (
     <header className={classNames(className, 'header_wrapper')}>
       <img src={PokeDexLogo} alt="PokeDex!" title="Logo do site" />
+
+      {children}
     </header>
   );
 };
 
 Header.propTypes = {
   className: PropTypes.string,
+  children: PropTypes.node,
 };
 
 Header.defaultProps = {
